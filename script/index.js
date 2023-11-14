5b90b212f0b1dc530eb00d49a9a4ba1b41d04d2c
Mzwa1080 <mshawnlouw@gmail.com>
2023-11-14 17:04:12 +0200

    loop challenge

---
+++ script/index.js
@@ -0,0 +1,20 @@
+let numbers=[12, 18, 11, 5, 8]
+
+function challenge(){
+    
+        if(numbers.length%2==0 ){
+            console.log( numbers[(numbers.length)/2-1], numbers[numbers.length/2]);
+            document.getElementById('output').textContent = 
+            new Array().concat(numbers[(numbers.length)/2-1], numbers[numbers.length/2]);
+
+        }
+        else{
+            console.log(numbers[parseInt(numbers.length/2)]);
+            document.getElementById('output').textContent = numbers[parseInt(numbers.length/2)];
+        }
+    
+    
+    
+}
+
+challenge()
